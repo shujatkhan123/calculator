1bccd19c493a6d68e5e207b2bb499396f640539e
shujat khan <shujatullahkhan999@gmail.com>
2021-07-20 15:56:30 +0500

    calculator2

---
+++ aap.js
@@ -0,0 +1,28 @@
+
+
+
+function sum(num){
+    
+var a = document.getElementById('cal');
+
+    a.value += num; 
+}
+
+
+function min(){
+
+    var a = document.getElementById('cal')
+    a.value = eval(a.value);
+
+}
+
+
+
+function reset(){
+
+    var a = document.getElementById('cal')
+    a.value = ""
+
+}
+
+
